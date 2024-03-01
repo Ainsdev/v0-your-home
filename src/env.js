4ff558ff58a4937ec6b09dca2d23f2d4d7dc154d
Ainsdev <ainsn1654@gmail.com>
2024-02-29 23:17:29 -0300

    fix

--- src/env.js
+++ src/env.js
@@ -29,7 +29,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_APP_URL: z.string(),
   },
 
   /**
